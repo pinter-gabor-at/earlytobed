5d2e8fa44820b15a430c3479ae656db74f41cd71
Pintér Gábor <pinter.gabor@gmx.at>
2025-04-11 20:48:34 +0200

    Minor changes.

--- src/main/java/eu/pintergabor/earlytobed/Global.java
+++ src/main/java/eu/pintergabor/earlytobed/Global.java
@@ -1,24 +1,44 @@
 package eu.pintergabor.earlytobed;
 
-import net.minecraft.resources.ResourceLocation;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.minecraft.resources.ResourceLocation;
 
 
 public final class Global {
 
-	// Used for logging and registration
+	private Global() {
+		// Static class.
+	}
+
+	/**
+	 * Used for logging and registration.
+	 */
 	public static final String MODID = "earlytobed";
-	// This logger is used to write text to the console and the log file.
+
+	/**
+	 * This logger is used to write text to the console and the log file.
+	 */
 	@SuppressWarnings("unused")
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	/**
+	 * Create a mod specific name.
+	 *
+	 * @param path Name without {@link #MODID}.
+	 */
+	@SuppressWarnings("unused")
+	public static String modName(String path) {
+		return MODID + ":" + path;
+	}
+
+	/**
 	 * Create a mod specific identifier.
 	 *
-	 * @param path Name, as in lang/*.json files without "*.modid." prefix.
+	 * @param path Name without {@link #MODID}.
 	 */
+	@SuppressWarnings("unused")
 	public static ResourceLocation modId(String path) {
 		return ResourceLocation.fromNamespaceAndPath(MODID, path);
 	}
