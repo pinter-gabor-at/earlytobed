ac26f3db925f27d44bcd258370a19ecaf508dcd9
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-16 12:31:06 +0100

    Bump versions.

--- src/main/java/eu/pintergabor/earlytobed/Global.java
+++ src/main/java/eu/pintergabor/earlytobed/Global.java
@@ -1,25 +1,49 @@
 package eu.pintergabor.earlytobed;
 
-import net.minecraft.resources.ResourceLocation;
-
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.minecraft.resources.Identifier;
 
 
 public final class Global {
 
-	// Used for logging and registration.
+	private Global() {
+		// Static class.
+	}
+
+	/**
+	 * Used for logging and registration.
+	 */
 	public static final String MODID = "earlytobed";
-	// This logger is used to write text to the console and the log file.
+
+	/**
+	 * This logger is used to write text to the console and the log file.
+	 */
 	@SuppressWarnings("unused")
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	/**
+	 * Create a mod specific name.
+	 *
+	 * @param path Name without {@link #MODID}.
+	 */
+	@Contract(pure = true)
+	@SuppressWarnings("unused")
+	public static @NotNull String modName(String path) {
+		return MODID + ":" + path;
+	}
+
+	/**
 	 * Create a mod specific identifier.
 	 *
-	 * @param path Name, as in lang/*.json files without "*.modid." prefix
+	 * @param path Name without {@link #MODID}.
 	 */
-	public static ResourceLocation modId(String path) {
-		return ResourceLocation.fromNamespaceAndPath(MODID, path);
+	@Contract("_ -> new")
+	@SuppressWarnings("unused")
+	public static @NotNull Identifier modId(String path) {
+		return Identifier.fromNamespaceAndPath(MODID, path);
 	}
 }
